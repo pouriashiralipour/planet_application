cf63a2e23e77d2085c3cca7355ae96135f117f15
pouriashiralipour <pouriashiralipour@gmail.com>
2025-08-30 01:52:50 +0330

    add size config

--- lib/screens/on_boarding_screen.dart
+++ lib/screens/on_boarding_screen.dart
@@ -5,6 +5,14 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('data')));
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.only(top: 20.0, right: 20.0),
+          child: Text('رد کردن'),
+        ),
+      ),
+    );
   }
 }
